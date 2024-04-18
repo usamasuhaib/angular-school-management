ab0d4ac36a645094f98307389f8dfa3f46578659
Usama Suhaib <usamabalti3377@gmail.com>
2024-04-18 17:35:24 +0500

    some modifications in user profile section

--- src/app/dashboard/navbar/navbar.component.ts
+++ src/app/dashboard/navbar/navbar.component.ts
@@ -1,20 +1,48 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from '../../shared/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-constructor(private router:Router){
+constructor(private router:Router, private userService:UserService){
 
+}
+
+userProfile: any;
+ngOnInit(): void {
+  this.loadUserProfile();
 }
 
 onAccount(){
   this.router.navigate(['/account'])
 
 }
+
+
+loadUserProfile(): void {
+  this.userService.getUserProfile().subscribe(
+    (data) => {
+      this.userProfile = data;
+    },
+    (error) => {
+      console.error('Failed to load user profile:', error);
+    }
+  );
+}
+
+
+
+
+
+
+
+
+
 }
